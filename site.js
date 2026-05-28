ca22447034d4107707bab6a8480bf5006bcf3d0c
Estela Beatriz <estelabeatriz149@gmail.com>
2026-05-27 21:33:29 -0300

    finalizando site de vendas

---
+++ site.js
@@ -0,0 +1,11 @@
+
+function abrirModal(titulo, texto) {
+  document.getElementById('modal-titulo').textContent = titulo;
+  document.getElementById('modal-texto').textContent = texto;
+  document.getElementById('fundo-modal').classList.add('ativo');
+}
+
+// Fecha o modal
+function fecharModal() {
+  document.getElementById('fundo-modal').classList.remove('ativo');
+}
